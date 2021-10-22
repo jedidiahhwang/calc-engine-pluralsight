cf7b33516485f0f9864c31f856653b6170b6d502
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-10-22 14:14:11 -0600

    compressed execute() functionality into the class method, created instances of MathEquation class

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -11,22 +11,27 @@ public class Main {
 
     static void performCalculations() {
     	// Currently takes in hard-coded values and does the calculation
-		double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-		double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-		char[] opCodes = {'d', 'a', 's', 'm'};
+		MathEquation[] equations = new MathEquation[4]; // Creates an array of MathEquation references
+		equations[0] = create(100.0d, 50.0d, 'd');
+		equations[1] = create(25.0d, 92.0d, 'a');
+		equations[2] = create(225.0d, 17.0d, 's');
+		equations[3] = create(11.0d, 3.0d, 'm');
 
-		// These are parallel arrays, meaning each array value has a corresponding value from another array.
-		double[] results = new double[opCodes.length];
-
-		for(int i = 0; i < opCodes.length; i++) {
-			results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-		}
-		for(double currentResult: results) {
-			System.out.println("result = " + currentResult);
+		for(MathEquation equation: equations) {
+			equation.execute(); // This is calling the execute() method made in MathEquation class
+			System.out.println("result = " + equation.result);
 		}
 	}
 
-    static void executeInteractively() {
+	private static MathEquation create(double leftVal, double rightVal, char opCode) {
+		MathEquation equation = new MathEquation();
+		equation.leftVal = leftVal;
+		equation.rightVal = rightVal;
+		equation.opCode = opCode;
+		return equation;
+	}
+
+	static void executeInteractively() {
     	// Takes input from command line
     	System.out.println("Enter an operation and two numbers:");
 		Scanner scanner = new Scanner(System.in);
